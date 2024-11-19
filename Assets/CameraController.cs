2d672e8841651b1c0ce2491ee1a33230fd8354fc
wiktoria <wiktoria.maj04@outlook.com>
2024-11-19 11:07:19 +0100

    oswietlenie

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class CameraController : MonoBehaviour
     public movementController playerController;
     // public Camera cam;
     Vector3 offset;
-    
+    private float alpha = 0;
+    private float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +26,23 @@ public class CameraController : MonoBehaviour
     void Update()
     {
        transform.position = offset + player.transform.position;
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.rotation = Quaternion.Euler(40, y-180, 0);
+        //}
+
+        float angle = speed * Time.deltaTime;
+        alpha = alpha + angle;
+        angle = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(40, -alpha, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(40, alpha, 0);
+        }
     }
 }
